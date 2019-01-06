614a3f15f20543d94e99abd8e5515e712780cc4e
hijackdude <1399128254@qq.com>
2019-01-06 23:51:14 +0800

    2016DJ_FINAL PROJECY_Vickers
    MOVIE GROSS

---
+++ bar.js
@@ -0,0 +1,26 @@
+var app = {
+	init: function(){
+		this.cacheDOM();
+		this.handleCharts();
+	},
+	cacheDOM: function(){
+		this.$chart = $(".bar-chart");
+	},
+	cssSelectors: {
+		chartBar: "bar-chart--inner"
+	},
+	handleCharts: function(){
+	
+	$.each(this.$chart, function(){
+	var $this = $(this),
+	total = $this.data("total"),
+	$targetBar = $this.find("."+app.cssSelectors.chartBar);
+	$targetBar.css("width","0%"); // zero out for animation
+	setTimeout(function(){
+	$targetBar.css("width",total+"%");
+	},400);
+		});
+	}
+}
+
+app.init();
